569b9367c524f6f8983e292de7aa3ddc060589cf
Aanjaneya24 <pandeyaanjaneya76@gmail.com>
2025-07-16 20:18:19 +0530

    Updates

---
+++ precache-manifest.fa9d53b2bccf2337542fbfa9a881e87c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67aeafc31a67a5b478ff6b2801151fe",
+    "url": "/atmosense/index.html"
+  },
+  {
+    "revision": "a3d4a5324300abadacd0",
+    "url": "/atmosense/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "c0499e7df1d46c6637c6",
+    "url": "/atmosense/static/css/main.3cbfd88f.chunk.css"
+  },
+  {
+    "revision": "a3d4a5324300abadacd0",
+    "url": "/atmosense/static/js/2.f9b8834c.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/atmosense/static/js/2.f9b8834c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0499e7df1d46c6637c6",
+    "url": "/atmosense/static/js/main.5f9dc675.chunk.js"
+  },
+  {
+    "revision": "e6f107e6511ffe0b6d82",
+    "url": "/atmosense/static/js/runtime-main.e72ddac9.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/atmosense/static/media/WeatherIcons.0a218296.gif"
+  }
+]);
